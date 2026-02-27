c37d0aad60cef406284d6516c6ea34d12814afaa
Moonenm <u0157634@thomasmore.be>
2026-02-27 14:06:21 +0100

    wijzigingen les 27-2

---
+++ programmingessentials/voorbeelden/Oefenles27-2/Oefenles27-2-selectie/Program.cs
@@ -0,0 +1,54 @@
+﻿double prijs = 27.5;
+
+if(prijs > 20)
+{
+   Console.WriteLine("Groter dan 20"); 
+} 
+
+string naam = "Maarten";
+
+// || wil zeggen of
+if(naam == "Maarten" || naam == "Sean") 
+    Console.WriteLine("De naam is Maarten of Sean");
+else if(naam == "Arthur")
+    Console.WriteLine("De naam is Arthur");
+else
+    Console.WriteLine("De naam is iets anders");
+
+// initialiseren
+int teller = 0;
+
+// Controleer voorwaarde
+while (teller < 10)
+{
+    // voer code uit
+    Console.WriteLine($"De teller is {teller}");
+
+    // Verhoog teller
+    teller++;
+}
+
+Console.WriteLine($"De teller is {teller}");
+
+// initialiseren ; Controleer voorwaarde ; verhoog teller
+for(int i = 0; i < 10 ; i++)
+{
+    Console.WriteLine($"De teller is {i}");
+}
+// i is niet bekend buiten de loop
+Console.WriteLine($"De teller is {i}");
+
+
+// opletten dat je de juiste tellers aanpast in de juiste lus!!
+int teller2 = 0, teller3 = 10;
+while(teller2 < 10)
+{
+    Console.WriteLine($"{teller2}");
+    while(teller3 > 0)
+    {
+        Console.WriteLine($"{teller3}");
+        teller3--;
+    }
+    teller3++;
+}
+
---
+++ programmingessentials/voorbeelden/Oefenles27-2/Oefenles27-2/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+int getal, leeftijd, huisnummer, saldo; //gehele getallen 12 356
+string naam, adres, voornaam, diploma; // eender welke tekst "Straat 15"
+double prijs; 
+bool isGelijk;
+char letter;
+DateTime geboorteDatum;
+
+Console.Write("Geef het adres: ");
+adres = Console.ReadLine(); // "straat 15" Console.ReadLine() geeft altijd een string terug
+
+Console.Write("Geef de leeftijd: ");
+leeftijd = Convert.ToInt32(Console.ReadLine()); 
+// ToInt16 --> -32K tot +32K 
+// ToInt32 --> -2 miljard tot + 2 miljard
+// afhankelijk van wat je verwacht als input.
+
+// in plaats van Convert.To... kun je ook 'datatype'.Parse gebruiken
+leeftijd = int.Parse(Console.ReadLine());
+prijs = double.Parse(Console.ReadLine());
+letter = char.Parse(Console.ReadLine());
+// leeftijd, prijs en letter zijn variabelen
+
+// Elke methode heeft haakjes! Niet elke methode heeft parameters(argumenten)
+Console.ReadLine();
+// "Tekst" is een parameter
+Console.WriteLine("Tekst");
+// invoer en out getal zijn parameters 
+// int.TryParse(invoer, out getal)
+naam = Console.ReadLine().ToUpper();
+
+// elke bewerking met een variabele, die niet ingegeven is door een gebruiker, vereist een initialisatie 
+string naam;
+// Onderstaande lijn werkt niet omdat naam null is. Daarom initialiseren --> string naam = "Maarten"
+Console.WriteLine(naam);
+
+
+
+Console.ReadLine();
